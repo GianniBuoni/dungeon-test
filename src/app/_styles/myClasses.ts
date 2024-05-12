cc221fe5a87e7c6846d84499c8170b80095059a5
buongian <162598175+GianniBuoni@users.noreply.github.com>
2024-05-12 13:01:00 -0400

    styling

---
+++ src/app/_styles/myClasses.ts
@@ -0,0 +1,10 @@
+import clsx from "clsx";
+
+export const myCard = clsx({
+  "bg-base-200 p-5 rounded-md shadow-sm": true,
+});
+
+export const myButton = clsx({
+  "btn btn-base-900 btn-sm rounded-md hover:btn-neutral transition-colors":
+    true,
+});
